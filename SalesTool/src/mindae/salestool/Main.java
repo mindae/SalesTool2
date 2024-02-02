417bef1d6b856b9fdc89115783515470d8cbec6f
mindae <MindaeLaddoo01>
2024-02-02 14:58:06 +0530

    fixed case

--- SalesTool/src/mindae/salestool/Main.java
+++ SalesTool/src/mindae/salestool/Main.java
@@ -9,8 +9,11 @@ public class Main {
 	}
 
 	private static void displayGreeting() {
-		System.out.println("HELLO HAPPY SALES PEOPLE!");
-		System.out.println("THIS APP SHOWS SALES DATA");
+		System.out.println("Hello Happy Sales People!");
+		System.out.println("This App Shows Sales Data");
+		System.out.println("--------------------------");
+		
+		
 		System.out.println("test 0");
 		System.out.println("test 2");
 	}
