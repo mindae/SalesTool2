f41e6cdd014daf736d382024f7c85a810e891d44
mindae <MindaeLaddoo01>
2024-02-02 14:52:03 +0530

    partial impl of displayData function

--- SalesTool/src/mindae/salestool/SalesData.java
+++ SalesTool/src/mindae/salestool/SalesData.java
@@ -3,6 +3,8 @@ package mindae.salestool;
 public class SalesData {
 	private int data[] = {0,4,42};
 	public void display() {
-		System.out.println("TODO: Implement data display");
+		for (int i = 0; i < data.length; i++) {
+			System.out.println("");
+		}
 	}
 }
