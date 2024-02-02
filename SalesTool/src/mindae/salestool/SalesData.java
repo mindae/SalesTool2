9a7a8b55dbce7d57cd8c52c1fe32c48615b9d1e0
mindae <MindaeLaddoo01>
2024-02-02 15:07:25 +0530

    complete updating sales data obj

--- SalesTool/src/mindae/salestool/SalesData.java
+++ SalesTool/src/mindae/salestool/SalesData.java
@@ -3,8 +3,9 @@ package mindae.salestool;
 public class SalesData {
 	private int data[] = {0,4,42};
 	public void display() {
+		System.out.println("Data: ");
 		for (int i = 0; i < data.length; i++) {
-			System.out.println("");
+			System.out.println("Next Value: "+data[i]);
 		}
 	}
 }
