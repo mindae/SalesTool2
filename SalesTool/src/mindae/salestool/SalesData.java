3b6b0dfb2f44c2e229add99ef5aefc5697e20d78
mindae <mahendrachaurasia@gmail.com>
2024-02-03 17:58:16 +0530

    Update SalesData.java
    summed up values

--- SalesTool/src/mindae/salestool/SalesData.java
+++ SalesTool/src/mindae/salestool/SalesData.java
@@ -1,11 +1,17 @@
 package mindae.salestool;
 
+/*
+* Represents the sales data
+*/
 public class SalesData {
 	private int data[] = {0,4,42};
 	public void display() {
 		System.out.println("Data: ");
+		int sum=0;
 		for (int i = 0; i < data.length; i++) {
 			System.out.println("Next Value: "+data[i]);
+			sum+=i;
 		}
+		System.out.println("Sum is: "+sum);
 	}
 }
