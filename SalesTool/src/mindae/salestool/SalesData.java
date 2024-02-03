8c37c9edf529a2d49cb63869307cd90b0fcefbfc
daisy <daisy@localhost>
2024-02-03 17:38:59 +0530

    changed defaults and refactored for loop

--- SalesTool/src/mindae/salestool/SalesData.java
+++ SalesTool/src/mindae/salestool/SalesData.java
@@ -1,10 +1,11 @@
 package mindae.salestool;
 
 public class SalesData {
-	private int data[] = {0,4,42};
+	private int data[] = {-1, 0, 4, 42, 84};
 	public void display() {
 		System.out.println("Data: ");
-		for (int i = 0; i < data.length; i++) {
+		for(int i: data) {
+			
 			System.out.println("Next Value: "+data[i]);
 		}
 	}
